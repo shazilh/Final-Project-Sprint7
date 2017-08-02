2aed793ce6f8566894ac7a722efceafdf86e06f4
Shazil Tovar <shazil@gmail.com>
2017-08-02 00:14:52 -0500

    primer intento de handlebars

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,2 +1,14 @@
+var template =Handlebars.templates["index"];
 
+        var data={};
+
+        Handlebars.registerHelper("linkto", function(url,name){
+            var name= Handlebars.Utils.escapeExpression(name),
+                  url= Handlebars.Utils.escapeExpression(url),
+                  link="<a href=\'" + location.protocol +"//" + location.host + "/" + url + ".html\'>" + name + "</a>";
+                return new Handlebars.SafeString(link);
+        });
+
+       //var compiladoHTML=compilar(data);
+        document.getElementById("action").innerHTML += template(data);
        
